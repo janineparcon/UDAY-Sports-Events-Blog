339270870631a0673f7e6ef8a42575657097384c
janineparcon <43872185+janineparcon@users.noreply.github.com>
2018-10-09 17:02:55 +0800

    Update news.php

--- news.php
+++ news.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>USEB Home</title>
+	<title>News</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -15,14 +15,6 @@
 					<li class="active"><a href="news.php">News</a></li>
 				<li><a href="about.php">About</a></li>
 				</ul>
-				<div class="nav-login">
-					<form>
-						<input type="text" name="uid" placeholder="Username/Email">
-						<input type="password" name="pwd" placeholder="Password">
-						<button type="submit" name="submit">Login</button>
-					</form>
-					<a href="signup.php">Sign Up</a>
-				</div>
 			</div>
 		</nav>
 	</header>
