c264e85a320df3bf23fa8724df9bc1d1bc64afce
janineparcon <43872185+janineparcon@users.noreply.github.com>
2018-10-09 17:02:06 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,31 @@
-<?php
-	include_once 'header.php';
-?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="news.php">News</a></li>
+				<li><a href="about.php">About</a></li>
+				</ul>
+				<div class="nav-login">
+					<form>
+						<input type="text" name="uid" placeholder="Username/Email">
+						<input type="password" name="pwd" placeholder="Password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">Sign Up</a>
+				</div>
+			</div>
+		</nav>
+	</header>
 
 	<section class="main-container">
 		<div class="main-wrapper">
@@ -16,6 +41,5 @@
 		</div>
 	</section>
 
-	<?php
-	include_once 'footer.php';
-?>
+</body>
+</html>
