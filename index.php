9ff67de8464f79bf091b7eee22389f20558daac3
janineparcon <43872185+janineparcon@users.noreply.github.com>
2018-10-09 17:01:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
-<?php
-	include_once 'header.php';
-?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UDAY Sports Events</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="news.php">News</a></li>
+				<li><a href="about.php">About</a></li>
+				</ul>
+				<div class="nav-login">
+					<form>
+						<input type="text" name="uid" placeholder="Username/Email">
+						<input type="password" name="pwd" placeholder="Password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">Sign Up</a>
+				</div>
+			</div>
+		</nav>
+	</header>
 
 	<section class="main-container">
 		<div class="main-wrapper">
@@ -8,6 +33,12 @@
 		</div>
 	</section>
 
-	<?php
-	include_once 'footer.php';
-?>
+	<div id ="slider">
+	<figure>
+		<img src="PICTURES/FootballBoys.jpg">
+		<img src="PICTURES/FootballGirls.jpg">
+	</figure>
+	</div>
+
+	</body>
+</html>
