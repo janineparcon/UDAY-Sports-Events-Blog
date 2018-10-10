1015cc1311745f14766f12feca59894a92ed542b
janineparcon <43872185+janineparcon@users.noreply.github.com>
2018-10-10 12:49:20 +0800

    Update dbh-inc.php

--- dbh-inc.php
+++ dbh-inc.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect("localhost","root","");
 //Check connection
 if (mysqli_connect_errno())
 {
-	echo "Failed to connect to MYSQL: " . myaqli_connect_error();
+	echo "Failed to connect to MYSQL: " . mysqli_connect_error();
 }
 else {
 	echo "You have successfully connected to MYSQL";
